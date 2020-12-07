1718a369e2d20dbc18b2010522fcd09645355a5b
Tetiana Serdiuk <tetianaskh@gmail.com>
2020-12-07 19:24:35 +0200

    Initial commit

---
+++ src/com/company/Task4.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Task4 {
+
+    // Обязательные задачи:
+
+    // Задача 1.
+    static void exercise01() {
+
+    }
+    //Задача 2.
+    static void exercise02() {
+
+    }
+    //
+    static void exercise03() {
+
+    }
+    //
+    static void exercise04() {
+
+    }
+
+    //
+    static void exercise001() {
+
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
