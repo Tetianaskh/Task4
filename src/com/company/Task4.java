b0ab49cbef6a75074ae27a7b634b0f54becf2841
Tetiana Serdiuk <tetianaskh@gmail.com>
2020-12-10 17:24:59 +0200

    Основные задачи: 1, 2, 4

--- src/com/company/Task4.java
+++ src/com/company/Task4.java
@@ -7,16 +7,16 @@ public class Task4 {
     // Обязательные задачи:
 
     // Задача 1. (Используя цикл while найти наибольшее из 10 чисел, вводимых пользователем)
-    static void exercise01() {
+    public static void exercise01() {
         System.out.println("Добрый день!");
         System.out.println("Наш помощник вычислит максимальное из десяти введенных чисел:");
         Scanner sc = new Scanner(System.in);
         int i = 0;
         int max = 0;
-        while (i<10) {
+        while (i < 10) {
             System.out.println("Введите, пожалуйста, число:");
             int number = sc.nextInt();
-            if (max < number) {
+            if (max < number || i == 0) {
                 max = number;
             }
             i++;
@@ -30,7 +30,7 @@ public class Task4 {
         System.out.println("Наш помощник расчитает сумму всех нечетных чисел из интервала 3....100:");
         int i = 3;
         int sum = 0;
-        while (i<100) {
+        while (i < 100) {
             int number = i;
             if (number % 2 != 0) {
                 sum += number;
@@ -43,33 +43,47 @@ public class Task4 {
     // Задача 3. (Пользователь вводит координаты центра окружности и ее радиус после чего ользователь вводит
     // координаты 5ти точек, определить сколько из них лежат в окружности (для определения рассеяний и проверок
     // сделать отдельные методы))
-    static void exercise03() {
+    public static void exercise03() {
 
     }
 
     // Задача 4. (Написать метод, который принимает число и возвращает true если оно простое и false - если нет)
-    public static boolean primeNumber(int numbers) {
-        return (numbers > 1) && (numbers % 1 == 0 || numbers % numbers == 0);
+    public static boolean isPrimeNumber (int number) {
+        int i = 2;
+        while (i < (number - 1)) {
+            if (number % i != 0) {
+                return false;
+            }
+            i++;
+        }
+        return true;
     }
-//    System.out.printLn("Добрый день!");
-//    System.out.printLn("Наш помощник определит является ли введенное Вами число простым");
-//    Scanner sc = new Scanner(System.in);
-//    System.out.printLn("Введите, пожалуйста, число:");
-//    int num = sc.nextInt();
-//    if(primeNumber(num)){
-//        System.out.println("Введенное число является простым!");
-//    } else {
-//        System.out.println("Введенное число Не является простым!");
-//    }
+
+    public static void exercise04() {
+        System.out.println("Добрый день!");
+        System.out.println("Наш помощник определит является ли введенное Вами число простым");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите, пожалуйста, число:");
+        int number = sc.nextInt();
+        if (isPrimeNumber(number)){
+        System.out.println("Введенное число является простым!");
+        } else {
+            System.out.println("Введенное число НЕ является простым!");
+        }
+    }
 
     // Дополнительные задачи:
 
-    // Задача 1. (Написать метод, который принимает роизвольно целое число и возвращает сумму всех его цифр)
-    static void exercise001() {
+    // Задача 1. (Написать метод, который принимает произвольно целое число и возвращает сумму всех его цифр)
+    public static void test () {
+
+    }
+
+    public static void exercise001 () {
 
     }
 
     public static void main(String[] args) {
-        exercise02();
+        exercise04();
     }
 }
